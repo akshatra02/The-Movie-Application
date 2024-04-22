abf64412ed9110a1e577da2d35d1a062798e6b26
akshatra02 <kungumaakshatra@gmail.com>
2024-04-22 13:51:34 +0530

    Solve issue in search query

--- app/src/main/java/com/example/themovieapp/presentation/screen/SearchScreen.kt
+++ app/src/main/java/com/example/themovieapp/presentation/screen/SearchScreen.kt
@@ -1,6 +1,5 @@
 package com.example.themovieapp.presentation.screen
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -11,37 +10,28 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material.icons.filled.Search
-import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.style.TextOverflow
@@ -51,12 +41,10 @@ import androidx.navigation.NavController
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.example.themovieapp.R
-import com.example.themovieapp.data.source.remote.MoviesApi
 import com.example.themovieapp.domain.model.Movie
-import com.example.themovieapp.presentation.components.BottomTab
 import com.example.themovieapp.presentation.components.Header
-import com.example.themovieapp.presentation.components.loadingitems.LoadingRowCard
 import com.example.themovieapp.presentation.components.RatingBar
+import com.example.themovieapp.presentation.components.loadingitems.LoadingRowCard
 import com.example.themovieapp.presentation.navigation.Screen
 import com.example.themovieapp.presentation.viewmodel.MovieDetailsViewModel
 import com.example.themovieapp.presentation.viewmodel.SearchMovieViewModel
@@ -77,9 +65,7 @@ fun SearchScreen(
     }
     val searchResult by searchViewModel.searchResults.collectAsState()
     val keyboardController = LocalSoftwareKeyboardController.current
-    val searchQuery = remember {
-        searchViewModel.searchQuery
-    }
+    val searchQuery = searchViewModel.searchQuery
     Header(title = R.string.search, navController = navController, tabPage = tabPage)
     { paddingValues ->
         val isEmpty = searchResult.isEmpty()
