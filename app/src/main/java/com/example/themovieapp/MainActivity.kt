4f8597e0dd837fced4e5c7bec9adaaa91382f039
akshatra02 <kungumaakshatra@gmail.com>
2024-04-24 17:45:17 +0530

    feat: Enhance app for TV experience

--- app/src/main/java/com/example/themovieapp/MainActivity.kt
+++ app/src/main/java/com/example/themovieapp/MainActivity.kt
@@ -1,18 +1,13 @@
 package com.example.themovieapp
 
 import android.os.Bundle
-import android.preference.PreferenceDataStore
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.themovieapp.presentation.navigation.Navigation
-import com.example.themovieapp.presentation.screen.HomeScreen
 import com.example.themovieapp.presentation.ui.theme.TheMovieAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -22,7 +17,6 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             TheMovieAppTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
